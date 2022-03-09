8b7e437312f8c119eb54312c7746c9b55f9cc822
YagoLopolito <yagolopolito@gmail.com>
2022-03-09 02:42:09 -0300

    Initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package com.solvd.laba;
+
+class HelloWorld{
+
+    public static void main(String args[]){
+
+        System.out.println("Hello " + args[0] + " " + args[1] + "!");
+
+    }
+}
---
+++ HelloYagoLopolito/src/com/solvd/laba/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.solvd.laba;
+
+class HelloWorld{
+
+    public static void main(String args[]){
+
+        System.out.println("Hello Yago Lopolito!");
+
+    }
+}
